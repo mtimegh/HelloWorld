01940a5b326ebf426fc7ff68aa6ea057ea42e79b
mariusz <mariusz@E3490>
2022-10-31 15:08:30 +0100

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Math
+    { 
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        { 
+            return a - b;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace HelloWorld
+{   // PascalCase - kazdy wyraz w ciagu -> duza litera -> klasy/namespace/pliki
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 10;
+
+                     
+            
+
+            Math math = new Math();
+
+            double addResult = math.Add(a, b);
+            double subtractResult = math.Subtract(a, b);
+
+            Console.WriteLine(addResult.ToString());
+            Console.WriteLine(subtractResult.ToString());
+
+            
+        }
+    }
+}
+
